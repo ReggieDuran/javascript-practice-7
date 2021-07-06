e4d02e97c96146f7ac92ec0049b8f8b196fecca9
Reggie <reggieduran15@gmail.com>
2021-07-06 16:26:34 +0800

    convert some functions into classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,43 +1,83 @@
-const productList = {
-    products: [
-        {
-            title: 'A Pillow',
-            imageUrl: 'https://media.istockphoto.com/photos/pillow-isolated-on-white-background-picture-id899226398?k=6&m=899226398&s=612x612&w=0&h=JtsWJqDPEQGmJnqWCkgUcHGHhCmjId1OkELo-uVeY-o=',
-            price: 19.99,
-            description: 'A soft pillow!'
-        },
-        {
-            title: 'A Carpet',
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Wollteppich_1.jpg/1200px-Wollteppich_1.jpg',
-            price: 89.99,
-            description: 'A carpet which you might like - or not.'
-        },
-    ],
+class Product {
+    // title = 'DEFAULT';
+    // imageUrl;
+    // description;
+    // price;
+
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
+}
+
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+
+    render() {
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
+            <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+                <div class="product-item__content">
+                    <h2>${this.product.title}</h2>
+                    <h3>${this.product.price}</h3>
+                    <p>${this.product.description}</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        `;
+
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products = [
+        new Product(
+            'A Pillow', 
+            'https://media.istockphoto.com/photos/pillow-isolated-on-white-background-picture-id899226398?k=6&m=899226398&s=612x612&w=0&h=JtsWJqDPEQGmJnqWCkgUcHGHhCmjId1OkELo-uVeY-o=',
+            19.99,
+            'A soft pillow!'
+        ),
+        new Product(
+            'A Carpet', 
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Wollteppich_1.jpg/1200px-Wollteppich_1.jpg',
+            89.99,
+            'A carpet which you might like - or not.'
+        ),
+        // {
+        //     title: 'A Pillow',
+        //     imageUrl: 'https://media.istockphoto.com/photos/pillow-isolated-on-white-background-picture-id899226398?k=6&m=899226398&s=612x612&w=0&h=JtsWJqDPEQGmJnqWCkgUcHGHhCmjId1OkELo-uVeY-o=',
+        //     price: 19.99,
+        //     description: 'A soft pillow!'
+        // },
+        // {
+        //     title: 'A Carpet',
+        //     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Wollteppich_1.jpg/1200px-Wollteppich_1.jpg',
+        //     price: 89.99,
+        //     description: 'A carpet which you might like - or not.'
+        // },
+    ];
+    
+    constructor() {}
+
     render() {
         const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul'); 
         prodList.className = 'product-list';
         for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-                <div>
-                    <img src="${prod.imageUrl}" alt="${prod.title}" >
-                    <div class="product-item__content">
-                        <h2>${prod.title}</h2>
-                        <h3>${prod.price}</h3>
-                        <p>${prod.description}</p>
-                        <button>Add to Cart</button>
-                    </div>
-                </div>
-            `;
-
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
-            console.log(prodList);
-
         }
         renderHook.append(prodList);
     }
 }
 
+const productList = new ProductList();
 productList.render();
